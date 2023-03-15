76424324e532ad946ee21d2ee6e5aca9b22aed38
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2023-03-15 15:29:19 +0100

    close Collapse in about for specification technique

--- src/components/page_about.js
+++ src/components/page_about.js
@@ -17,7 +17,7 @@ function About() {
         <Collapse title="Fiabilité">
           <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
         </Collapse>
-        <Collapse title="Respect" isInitiallyOpen={true}>
+        <Collapse title="Respect" isInitiallyOpen={false}>
           <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
         </Collapse>
         <Collapse title="Service">
